b20dab8c57c89fa3977ef4185b1f112c2129b9f6
Haolader91 <haolader910@gmail.com>
2026-04-30 16:27:14 +0600

    first commit or Menu bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+import { BookOpen, ChevronDown, LogOut, User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import loginImage from "../Asstes/web Design and Developer.png";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/">About</Link>
+      </li>
+      <li>
+        <Link href="/">Contact</Link>
+      </li>
+    </>
+  );
+  return (
+    <nav className="bg-white border-b border-gray-200 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex items-center">
+            <Link href="/" className="text-2xl ">
+              <span className="text-[#6366F1]">Skill</span>Sphere
+            </Link>
+          </div>
+          <div>
+            <ul className="flex gap-10 text-gray-700 font-medium">{links}</ul>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <Link
+              href="/login"
+              className="text-gray-700 border border-gray-400 px-5 py-2 rounded-md  hover:text-[#6366F1] font-medium"
+            >
+              Login
+            </Link>
+            <Link
+              href="/register"
+              className="bg-[#6366F1] text-white px-5 py-2 rounded-md font-medium hover:bg-[#6366F1] transition"
+            >
+              Register
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

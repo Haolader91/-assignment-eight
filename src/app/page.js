cca280caec29e267293b6a92fb78a5e69917bfcd
Haolader91 <haolader910@gmail.com>
2026-05-01 19:41:37 +0600

    course card image issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroBanner from "@/components/Banner";
+import Courses from "@/components/Courses";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
       <HeroBanner />
+      <Courses></Courses>
     </div>
   );
 }
